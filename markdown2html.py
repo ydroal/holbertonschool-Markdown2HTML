fc699ac53d7c45397a898ce241867c13dae0651b
ydroal <droal.yoko@gmail.com>
2023-08-03 17:34:52 +0200

    update: markdown2html.py for task1

--- markdown2html.py
+++ markdown2html.py
@@ -13,11 +13,18 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     if len(args) < 3:
-      print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-      sys.exit(1)
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        sys.exit(1)
     elif not os.path.isfile(args[1]):
-      print(f'Missing {args[1]}', file=sys.stderr)
-      sys.exit(1)
+        print(f'Missing {args[1]}', file=sys.stderr)
+        sys.exit(1)
     else:
-      sys.exit(0)
-    
+        with open(args[2], mode='w') as out_f:
+            with open(args[1]) as in_f:
+                for line in in_f:
+                    level = line.count('#')
+                    if level != 0:
+                        line = line.replace('#', '').strip()
+                        converted_heading = f'<h{level}>{line}</h{level}>\n'
+                    out_f.write(converted_heading)
+        sys.exit(0)
